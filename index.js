5bb2dc19e7ffd42c312f3fae4e9265523b580021
Igor <igordg@mail.ru>
2020-04-03 13:19:11 +0300

    1.0.0

--- index.js
+++ index.js
@@ -95,15 +95,97 @@ function searchName() {
 }
 
 
-function searchName() {
+function searchTel() {
 
     var input, filter, table, tr, td, i;
-    input = document.getElementById("inputName");
+    input = document.getElementById("inputTel");
     filter = input.value.toUpperCase();
     table = document.getElementById("tableId");
     tr = table.getElementsByTagName("tr");
     for (i = 2; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[1];
+        td = tr[i].getElementsByTagName("td")[2];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+
+}
+
+function searchMob() {
+
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("inputMob");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tableId");
+    tr = table.getElementsByTagName("tr");
+    for (i = 2; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[3];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+
+}
+
+
+
+function searchEmail() {
+
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("inputEmail");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tableId");
+    tr = table.getElementsByTagName("tr");
+    for (i = 2; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[4];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+
+}
+
+function searchPosition() {
+
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("inputPosition");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tableId");
+    tr = table.getElementsByTagName("tr");
+    for (i = 2; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[5];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+
+}
+
+function searchDepartment() {
+
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("inputDepartment");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tableId");
+    tr = table.getElementsByTagName("tr");
+    for (i = 2; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[6];
         if (td) {
             if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
                 tr[i].style.display = "";
