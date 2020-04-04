86a83aaa0501e1644842dafa7da43788d292e711
Igor <igordg@mail.ru>
2020-04-04 19:33:29 +0300

    1.0.3

--- phone_book.js
+++ phone_book.js
@@ -1,4 +1,32 @@
 let arrPeople = [{
+    name: 'Вадим Стебаков',
+    phone: "23-43-43",
+    mobile: 89273543453,
+    email: 'corporation@mail.ru',
+    position: 'Начальник отдела',
+    department: 'Отдел разработки ПО'
+},{
+    name: 'Алексей Шульгин',
+    phone: "65-83-89",
+    mobile: 89141657879,
+    email: 'corporation@mail.ru',
+    position: 'Начальник отдела',
+    department: 'Отдел рыбной промышленности'
+}, {
+    name: 'Иван Марусеев',
+    phone: "78-45-23",
+    mobile: 8925343543,
+    email: 'corporation@mail.ru',
+    position: 'Начальник отдела',
+    department: 'Отдел взаимодействия со СМИ'
+}, {
+    name: 'Роман Власов',
+    phone: "78-93-90",
+    mobile: 89256544564,
+    email: 'corporation@mail.ru',
+    position: 'Начальник отдела',
+    department: 'Отдел правового обеспечения'
+}, {
     name: 'Петр Сергеев',
     phone: "78-55-67",
     mobile: 89141657879,
@@ -47,14 +75,7 @@ let arrPeople = [{
     email: 'corporation@mail.ru',
     position: 'Администратор',
     department: 'IT'
-}, {
-    name: 'Борис Веденский',
-    phone: "23-42-34",
-    mobile: 8925343543,
-    email: 'corporation@mail.ru',
-    position: 'Специалист',
-    department: 'Кадровое обеспечение'
-}, {
+},  {
     name: 'Константин Перцев',
     phone: "23-43-43",
     mobile: 89273543453,
@@ -69,74 +90,32 @@ let arrPeople = [{
     position: 'Инструктор',
     department: 'Маркетинг'
 }, {
-    name: 'Петр Сергеев',
-    phone: "78-55-67",
-    mobile: 89141657879,
-    email: 'corporation@mail.ru',
-    position: 'Начальник отдела',
-    department: 'IT'
-}, {
-    name: 'Сергей Васильев',
-    phone: "34-55-38",
-    mobile: 89256544564,
-    email: 'corporation@mail.ru',
-    position: 'Заместитель',
-    department: 'IT'
-}, {
-    name: 'Иван Пронин',
-    phone: "23-42-34",
-    mobile: 8925343543,
-    email: 'corporation@mail.ru',
-    position: 'Специалист',
-    department: 'Кадровое обеспечение'
-}, {
-    name: 'Василий Алтаев',
-    phone: "23-43-43",
-    mobile: 89273543453,
-    email: 'corporation@mail.ru',
-    position: 'Инженер',
-    department: 'Финансовая служба'
-}, {
-    name: 'Федор Константинополев',
-    phone: "45-43-39",
+    name: 'Андрей Мирославов',
+    phone: "78-98-45",
     mobile: 89255645656,
     email: 'corporation@mail.ru',
-    position: 'Инструктор',
-    department: 'Маркетинг'
+    position: 'Специалист',
+    department: 'Отдел взаимодействия со СМИ'
 }, {
-    name: 'Арсений Петров',
+    name: 'Борис Иванов',
     phone: "78-55-67",
     mobile: 89141657879,
     email: 'corporation@mail.ru',
     position: 'Инженер',
-    department: 'IT'
+    department: 'Отдел разработки ПО'
 }, {
-    name: 'Юрий Плотников',
+    name: 'Станислав Рогов',
     phone: "34-55-38",
     mobile: 89256544564,
     email: 'corporation@mail.ru',
-    position: 'Администратор',
-    department: 'IT'
+    position: 'Специалист',
+    department: 'Отдел правового обеспечения\''
 }, {
-    name: 'Борис Веденский',
+    name: 'Владлен Коробов',
     phone: "23-42-34",
     mobile: 8925343543,
     email: 'corporation@mail.ru',
     position: 'Специалист',
-    department: 'Кадровое обеспечение'
-}, {
-    name: 'Константин Перцев',
-    phone: "23-43-43",
-    mobile: 89273543453,
-    email: 'corporation@mail.ru',
-    position: 'Администратор',
-    department: 'Маркетинг'
-}, {
-    name: 'Владимир Песков',
-    phone: "45-43-39",
-    mobile: 89255645656,
-    email: 'corporation@mail.ru',
-    position: 'Инструктор',
-    department: 'Маркетинг'
-}
+    department: 'Отдел рыбной промышленности'
+},
 ]
