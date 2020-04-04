466d822f3e30b65a9f2d80cf2177f0f7c91eb48d
Igor <igordg@mail.ru>
2020-04-04 19:08:35 +0300

    1.0.2

--- phone_book.js
+++ phone_book.js
@@ -68,5 +68,75 @@ let arrPeople = [{
     email: 'corporation@mail.ru',
     position: 'Инструктор',
     department: 'Маркетинг'
+}, {
+    name: 'Петр Сергеев',
+    phone: "78-55-67",
+    mobile: 89141657879,
+    email: 'corporation@mail.ru',
+    position: 'Начальник отдела',
+    department: 'IT'
+}, {
+    name: 'Сергей Васильев',
+    phone: "34-55-38",
+    mobile: 89256544564,
+    email: 'corporation@mail.ru',
+    position: 'Заместитель',
+    department: 'IT'
+}, {
+    name: 'Иван Пронин',
+    phone: "23-42-34",
+    mobile: 8925343543,
+    email: 'corporation@mail.ru',
+    position: 'Специалист',
+    department: 'Кадровое обеспечение'
+}, {
+    name: 'Василий Алтаев',
+    phone: "23-43-43",
+    mobile: 89273543453,
+    email: 'corporation@mail.ru',
+    position: 'Инженер',
+    department: 'Финансовая служба'
+}, {
+    name: 'Федор Константинополев',
+    phone: "45-43-39",
+    mobile: 89255645656,
+    email: 'corporation@mail.ru',
+    position: 'Инструктор',
+    department: 'Маркетинг'
+}, {
+    name: 'Арсений Петров',
+    phone: "78-55-67",
+    mobile: 89141657879,
+    email: 'corporation@mail.ru',
+    position: 'Инженер',
+    department: 'IT'
+}, {
+    name: 'Юрий Плотников',
+    phone: "34-55-38",
+    mobile: 89256544564,
+    email: 'corporation@mail.ru',
+    position: 'Администратор',
+    department: 'IT'
+}, {
+    name: 'Борис Веденский',
+    phone: "23-42-34",
+    mobile: 8925343543,
+    email: 'corporation@mail.ru',
+    position: 'Специалист',
+    department: 'Кадровое обеспечение'
+}, {
+    name: 'Константин Перцев',
+    phone: "23-43-43",
+    mobile: 89273543453,
+    email: 'corporation@mail.ru',
+    position: 'Администратор',
+    department: 'Маркетинг'
+}, {
+    name: 'Владимир Песков',
+    phone: "45-43-39",
+    mobile: 89255645656,
+    email: 'corporation@mail.ru',
+    position: 'Инструктор',
+    department: 'Маркетинг'
 }
 ]
